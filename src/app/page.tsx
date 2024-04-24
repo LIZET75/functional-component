cc2a22b82d8b26cd93036f2c1ec6d4497a54b0b2
LIZET75 <lizet75@gmail.com>
2024-04-24 11:43:21 -0600

    now button increases age in tsx page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
-import React from "react";
+import React, { useState } from "react";
 
 // Pass in the string "Bob" as the 1st argument, the props argument. And pass in the number 30 as the value for the "initialAge" function
 // class UserProfile extends React.Component {
@@ -30,20 +31,29 @@ import React from "react";
 
 //Convert the class UserProfile as a functional component that takes 1 prop as an argument
 
-function UserProfile(props: any) {
+function UserProfile(props: { name: string; age:number}) {
+  // Use useState to manage the age state
+  const [age, setAge] = useState(props.age);
+
+
+  // Function to handle the click event and increase the age
+  const handleBirthday = () => {
+    setAge(prevAge => prevAge + 1);
+  };
+
+// function UserProfile(props: any) {
   return (
     <div>
     <h1> Profile </h1>
     <h1>{props.name}</h1>
-    <h1>{props.number}</h1>
-    <button>Happy Birthday!</button>
+    <h1>{age}</h1>
+    <button onClick={handleBirthday}>Happy Birthday!</button>
     </div>
   );
-}
-{/* <><UserProfile name="Bob" /><UserProfile number /></>30 /> */}
+} 
 
 export default function User() {
-  return <UserProfile name="Bob" number="30" />;
+  return <UserProfile name="Bob" age={30} />; // This passes the initial age as a prop
  }
 
  //need to export the function that has the values not the function that has the props.
